107425749dff7c43237e59e56a8ed1709bd5588b
Arina Gabulyan <arinagab@yandex.ru>
2016-10-21 00:28:22 +0300

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,17 @@
+from Get_id import *
+from Friends import *
+
+input_id = input('Введите id: ')
+user = GetID(input_id)
+user_id = user.execute()
+
+friends_client = GetFriends(user_id)
+friends = friends_client.execute()
+
+ages = []
+counts = []
+
+for (age, count) in friends:
+    print('{} {}'.format(int(age), count * '#'))
+    ages.append(int(age))
+    counts.append(int(count))
